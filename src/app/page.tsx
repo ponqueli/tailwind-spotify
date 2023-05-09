53143d332d0ac01c16e683d1f421036536bac8d2
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-05-09 19:02:24 -0300

    create sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,45 @@
+import { Home as HomeIcon, Search, Library } from 'lucide-react'
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">sidebar</aside>
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className='flex items-center gap-2 '>
+            <div className='w-3 h-3 bg-red-500 rounded-full' />
+            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
+            <div className='w-3 h-3 bg-green-500 rounded-full' />
+          </div>
+          <nav className='space-y-5 mt-6'>
+            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <HomeIcon />
+              Home
+            </a>
+            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+            </a>
+          </nav>
+
+
+          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>TudoJunto&Misturado</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Deep House 2023</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Dramatic Classical</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Baden</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Mozart for Babies</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Pop Goes Classical</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>ClassicsByJose</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Antigonas xD</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Classical Essentials</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Rosicrucian Meditation</a>
+            <a href="" className='flex items-center gap-3 text-sm text-zinc-400 hover:text-zinc-100'>Benedictus - Karl Jenkins</a>
+          </nav>
+        </aside>
         <main className="flex-1 p-6">main</main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
