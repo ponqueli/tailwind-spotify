a1aa8c7c85f911fbc71975ce97b6474c3d0b9193
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-05-10 17:18:34 -0300

    add cursor hover on the button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
           <div className='flex items-center gap-2 '>
-            <div className='w-3 h-3 bg-red-500 rounded-full' />
-            <div className='w-3 h-3 bg-yellow-500 rounded-full' />
-            <div className='w-3 h-3 bg-green-500 rounded-full' />
+            <button className='w-3 h-3 bg-red-500 rounded-full' />
+            <button className='w-3 h-3 bg-yellow-500 rounded-full' />
+            <button className='w-3 h-3 bg-green-500 rounded-full' />
           </div>
           <nav className='space-y-5 mt-6'>
             <a href="#" className='flex items-center gap-3 text-sm font-semibold text-zinc-300 hover:text-zinc-100'>
