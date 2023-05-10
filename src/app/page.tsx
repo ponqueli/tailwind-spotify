cb7e66e00457567faa6bae9c2f397e68acf4e7f2
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-05-10 17:16:27 -0300

    finishing footer portion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Heart, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Heart, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Volume2, Maximize2 } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -121,32 +121,34 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-6 flex items-center justify-between">
         <div className='flex items-center gap-3'>
           <Image src="/album.jpeg" width={56} height={56} alt="capa album era" />
           <div className='flex flex-col gap-1'>
             <strong className='font-normal'>Hymne</strong>
             <span className='text-xs text-zinc-400'>Era</span>
           </div>
-          <a className='ml-3' href='#'><Heart /></a>
+          <button className='ml-3 text-zinc-400 hover:text-green-400 transition-colors'>
+            <Heart size={20} />
+          </button>
         </div>
 
         <div className='flex flex-col items-center gap-2'>
           <div className='flex items-center gap-6'>
-            <button>
-              <Shuffle size={20} className='text-zinc-200' />
+            <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+              <Shuffle size={20} />
             </button>
-            <button>
-              <SkipBack size={20} className='text-zinc-200' />
+            <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+              <SkipBack size={20} />
             </button>
-            <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
+            <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black hover:bg-green-400 transition-colors'>
               <Play />
             </button>
-            <button>
-              <SkipForward size={20} className='text-zinc-200' />
+            <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+              <SkipForward size={20} />
             </button>
-            <button>
-              <Repeat size={20} className='text-zinc-200' />
+            <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+              <Repeat size={20} />
             </button>
           </div>
 
@@ -159,8 +161,27 @@ export default function Home() {
           </div>
         </div>
 
-        <div></div>
-      </footer>
-    </div>
+        <div className='flex items-center gap-4'>
+          <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+            <Mic2 size={20} />
+          </button>
+          <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+            <LayoutList size={20} />
+          </button>
+          <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+            <Laptop2 size={20} />
+          </button>
+          <button className='flex items-center gap-2 text-zinc-400 hover:text-zinc-100 transition-colors'>
+            <Volume2 size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="h-1 w-10 bg-zinc-200 rounded-full"></div>
+            </div>
+          </button>
+          <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+            <Maximize2 size={20} />
+          </button>
+        </div>
+      </footer >
+    </div >
   )
 }
